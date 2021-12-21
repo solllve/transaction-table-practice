f23a81323360d74d0af39278955773cc7757afd5
solllve <caseyhald@Caseys-MacBook-Pro.local>
2021-12-21 13:54:58 -0800

    installed and setup tailwind and redux

---
+++ client/components/table.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+function Table() {
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    );
+}
+
+export default Table;
