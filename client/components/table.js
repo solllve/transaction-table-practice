0b75efeef63b0a3f81de69825f979268ff38b9dc
solllve <caseyhald@Caseys-MacBook-Pro.local>
2021-12-23 11:19:32 -0800

    store data, return store

--- client/components/table.js
+++ client/components/table.js
@@ -6,11 +6,6 @@ import { validate, getAddressInfo } from 'bitcoin-address-validation';
 import Web3 from 'web3'
 const web3 = new Web3(new Web3.providers.HttpProvider('https://cloudflare-eth.com'));
 
-const people = [
-    { name: 'Jane Cooper', title: 'Regional Paradigm Technician', role: 'Admin', email: 'jane.cooper@example.com' },
-    // More people...
-]
-
 const getEthTimestamp = (blockNum) => {
     let blockParse = Number(blockNum)
     const block = web3.eth.getBlock(blockParse);
@@ -42,7 +37,7 @@ const formatCryptoData = (data) => {
         //if btc
         else if (validate(item.to) || validate(item.from)) {
             let btcFormat = Number(item.amountCrypto) / 100000000;
-            let btcFormatFixed = btcFormat.toFixed(8); 
+            let btcFormatFixed = btcFormat.toFixed(7); 
             item.amountCrypto = btcFormatFixed
         }
     })
@@ -65,7 +60,7 @@ const getDate = (item) => {
   
 const transactionFormat = (data) => {
     const rawData = data[0].concat(data[1], data[2]);
-    console.log(rawData);
+    //console.log(rawData);
     const cleanedData = []
     rawData.forEach(item => {
         cleanedData.push({
@@ -82,10 +77,11 @@ const transactionFormat = (data) => {
     sortByDecendingDate(cleanedData)
     formatDate(cleanedData)
     formatCryptoData(cleanedData)
-    console.log(cleanedData)
+    return cleanedData
 }
 
 const rawDataCount = (data) => {
+    //Real world I would do this better.
     const rawData = data[0].concat(data[1], data[2]);
     return rawData.length
 }
@@ -100,83 +96,40 @@ const Table = () => {
     const store = useSelector(state => state);
 
     useEffect(() => {
-
         Promise.all([
             fetch(server.ethApiUrl),
             fetch(server.custodialApiUrl),
             fetch(server.btcApiUrl)
         ]).then(function (responses) {
-            
             return Promise.all(responses.map(function (response) {
                 return response.json()
             }));
-
         }).then(function (data) {
-
-            transactionFormat(data)
-            
+            const transactions = transactionFormat(data)
+            //send cleaned data to redux store
+            dispatch(fetchApi(transactions))
+            //console.log(store);
         }).catch(function (error) {
             console.log(error);
+            
         });
-       
-    })
+    }, [dispatch]);
 
     return (
-        <div className="flex flex-col">
-        <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-            <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-            <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-                <table className="min-w-full divide-y divide-gray-200">
-                <thead className="bg-gray-50">
-                    <tr>
-                    <th
-                        scope="col"
-                        className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                    >
-                        Name
-                    </th>
-                    <th
-                        scope="col"
-                        className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                    >
-                        Title
-                    </th>
-                    <th
-                        scope="col"
-                        className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                    >
-                        Email
-                    </th>
-                    <th
-                        scope="col"
-                        className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                    >
-                        Role
-                    </th>
-                    <th scope="col" className="relative px-6 py-3">
-                        <span className="sr-only">Edit</span>
-                    </th>
-                    </tr>
-                </thead>
-                <tbody className="bg-white divide-y divide-gray-200">
-                    {people.map((person) => (
-                    <tr key={person.email}>
-                        <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">{person.name}</td>
-                        <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{person.title}</td>
-                        <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{person.email}</td>
-                        <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{person.role}</td>
-                        <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                        <a href="#" className="text-indigo-600 hover:text-indigo-900">
-                            Edit
-                        </a>
-                        </td>
-                    </tr>
-                    ))}
-                </tbody>
-                </table>
-            </div>
-            </div>
-        </div>
+        <div>
+            <ul>
+            {store.transactions.data.map(item => (
+                <li>
+                    {item.type}
+                    {item.to}
+                    {item.from}
+                    {item.amountFiat}
+                    {item.amountCrypto}
+                    {item.date}
+                </li>
+            ))}
+            </ul>
+            
         </div>
     )
 }
